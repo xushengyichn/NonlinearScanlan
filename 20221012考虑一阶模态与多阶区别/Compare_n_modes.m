0248fe7d5514b0db2b1eaaa54c33eb1bbca3f573
xushengyichn <54436848+xushengyichn@users.noreply.github.com>
2022-10-13 21:21:10 +0200

    修复并行bug

--- 20221012考虑一阶模态与多阶区别/Compare_n_modes.m
+++ 20221012考虑一阶模态与多阶区别/Compare_n_modes.m
@@ -25,44 +25,44 @@ numberofTMD = 1; % 所需要优化的TMD的数量
 
 % 读取梁桥模型
 
-% 将ANSYS中的稀疏矩阵处理为完全矩阵
-% Handling sparse matrices in ANSYS as full matrices 导入ANSYS MCK矩阵
-% Import MCK matrix from ANSYS
-hb_to_mm ('KMatrix.matrix', 'K.txt');
-hb_to_mm ('MMatrix.matrix', 'M.txt');
-% hb_to_mm ('CMatrix.matrix', 'C.txt');
+% % 将ANSYS中的稀疏矩阵处理为完全矩阵
+% % Handling sparse matrices in ANSYS as full matrices 导入ANSYS MCK矩阵
+% % Import MCK matrix from ANSYS
+% hb_to_mm ('KMatrix.matrix', 'K.txt');
+% hb_to_mm ('MMatrix.matrix', 'M.txt');
+% % hb_to_mm ('CMatrix.matrix', 'C.txt');
 
-%map the node and matrix from the KMatrix.mapping and MMatrix.mapping
-Kdata = importdata('K.txt').data;
-Kmatrix = zeros(Kdata(1, 1), Kdata(1, 2));
+% %map the node and matrix from the KMatrix.mapping and MMatrix.mapping
+% Kdata = importdata('K.txt').data;
+% Kmatrix = zeros(Kdata(1, 1), Kdata(1, 2));
 
-for i = 2:size(Kdata, 1)
-    Kmatrix(Kdata(i, 1), Kdata(i, 2)) = Kdata(i, 3);
-end
-
-Mdata = importdata('M.txt').data;
-Mmatrix = zeros(Mdata(1, 1), Mdata(1, 2));
-
-for i = 2:size(Mdata, 1)
-    Mmatrix(Mdata(i, 1), Mdata(i, 2)) = Mdata(i, 3);
-end
-
-% Cdata = importdata('C.txt').data;
-% Cmatrix_DP = zeros(Cdata(1, 1), Cdata(1, 2));
-%
-% for i = 2:size(Cdata, 1)
-%     Cmatrix_DP(Cdata(i, 1), Cdata(i, 2)) = Cdata(i, 3);
+% for i = 2:size(Kdata, 1)
+%     Kmatrix(Kdata(i, 1), Kdata(i, 2)) = Kdata(i, 3);
 % end
 
-% 还原对角线以上元素，使之为对称阵, ANSYS只给出下三角矩阵
-% Restore the elements above the diagonal to make it a symmetric matrix, ANSYS only gives the lower triangular matrix
-K = diag(diag(Kmatrix) / 2) + Kmatrix - diag(diag(Kmatrix));
-K = K + K';
-M = diag(diag(Mmatrix) / 2) + Mmatrix - diag(diag(Mmatrix));
-M = M + M';
-% C_exp = diag(diag(Cmatrix_DP) / 2) + Cmatrix_DP - diag(diag(Cmatrix_DP));
-% C_exp = C_exp + C_exp';
-% C =C_exp;
+% Mdata = importdata('M.txt').data;
+% Mmatrix = zeros(Mdata(1, 1), Mdata(1, 2));
+
+% for i = 2:size(Mdata, 1)
+%     Mmatrix(Mdata(i, 1), Mdata(i, 2)) = Mdata(i, 3);
+% end
+
+% % Cdata = importdata('C.txt').data;
+% % Cmatrix_DP = zeros(Cdata(1, 1), Cdata(1, 2));
+% %
+% % for i = 2:size(Cdata, 1)
+% %     Cmatrix_DP(Cdata(i, 1), Cdata(i, 2)) = Cdata(i, 3);
+% % end
+
+% % 还原对角线以上元素，使之为对称阵, ANSYS只给出下三角矩阵
+% % Restore the elements above the diagonal to make it a symmetric matrix, ANSYS only gives the lower triangular matrix
+% K = diag(diag(Kmatrix) / 2) + Kmatrix - diag(diag(Kmatrix));
+% K = K + K';
+% M = diag(diag(Mmatrix) / 2) + Mmatrix - diag(diag(Mmatrix));
+% M = M + M';
+% % C_exp = diag(diag(Cmatrix_DP) / 2) + Cmatrix_DP - diag(diag(Cmatrix_DP));
+% % C_exp = C_exp + C_exp';
+% % C =C_exp;
 %%
 % 模态矩阵计算
 
