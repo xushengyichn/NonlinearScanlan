6f6b4384c8847e4681100f1849a02fcae7686cb5
xushengyichn <54436848+xushengyichn@users.noreply.github.com>
2022-10-17 20:30:31 +0200

    设置阻尼比优化代码

---
+++ Optim_Damping_for_n_modes.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Author: xushengyichn 54436848+xushengyichn@users.noreply.github.com
+%Date: 2022-10-17 15:52:12
+%LastEditors: xushengyichn 54436848+xushengyichn@users.noreply.github.com
+%LastEditTime: 2022-10-17 16:34:15
+%FilePath: \NonlinearScanlan\Optim_Damping_for_n_modes.m
+%Description: 返回优化所需的多阶级模态最小阻尼
+%
+%Copyright (c) 2022 by xushengyichn 54436848+xushengyichn@users.noreply.github.com, All Rights Reserved. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [minDampingRatio,minDampingRatio_sys]=Optim_Damping_for_n_modes(mode_number,numberofTMD,mTMD,zetaTMD,fTMD,locationTMD,calmodes_all)
+    minDamp_sys=zeros(length(calmodes_all),1);
+    minDamp_aero=zeros(length(calmodes_all),1);
+    for k1 = 1: length(calmodes_all)
+        calmodes=calmodes_all(k1);
+        [result]=Compare_n_modes(mode_number,numberofTMD,mTMD,zetaTMD,fTMD,locationTMD,calmodes);
+        minDamp_sys(k1)=min(result.Mode_sys.("Damping ratio"));
+        minDamp_aero(k1)=min(result.Mode.("Damping ratio"));
+    end
+    minDampingRatio=min(minDamp_aero);
+    minDampingRatio_sys=min(minDamp_sys);
+end
